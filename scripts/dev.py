79aa80c4848acfa67ed29434b32c56e69d64bc1e
Philipp <wholecellmoonshot@gmail.com>
2026-04-22 11:53:56 +0000

    description

---
+++ scripts/dev.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import mash_model_1_0 as mm
+
+
+# In[9]:
+
+
+import mash_model_1_0 as mm
+
+# This is the path
+print(mm.config.proj_path)
+
+# This is a parameter from the YAML file
+print(mm.config.params["cycles"])
+print(mm.config.params["tests"])
+print(mm.config.params['project'])
+print(mm.config.params['tests'])
+
+
+# In[ ]:
+
+
+
+
